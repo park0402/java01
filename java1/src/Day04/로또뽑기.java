e411ac2e95389e2d003229b8e90d9ac533a0030c
504 <504@DESKTOP-QB6GEF7>
2022-03-04 17:35:55 +0900

    틱택토 로또

--- java1/src/Day04/로또뽑기.java
+++ java1/src/Day04/로또뽑기.java
@@ -1,5 +1,6 @@
 package Day04;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class 로또뽑기 {// cs
@@ -14,7 +15,6 @@ public class 로또뽑기 {// cs
 		3. 사용자번호 와 컴퓨터 추첨번호와 동일 수의 개수
 	 */
 	public static void main(String[] args) {// ms
-	
 		
 		// 준비
 		int[] 사용자번호 = new int[6]; // 사용자가 선택한 번호 목록 
@@ -24,7 +24,7 @@ public class 로또뽑기 {// cs
 		// 1. 사용자에게 입력받기 
 		for( int i = 0 ; i<6 ; i++ ) {
 			// i는 0부터 5까지 1씩증가반복 => 6회반복
-			System.out.print(" 1~45까지 번호를 6번넣어주세요 ");
+			System.out.print(" 1~45 사이 입력 : ");
 			int 번호 = scanner.nextInt();
 			
 			boolean 통과 = true; // 저장 되는 조건 변수
@@ -46,7 +46,7 @@ public class 로또뽑기 {// cs
 					통과 = false; // 저장x
 				} // if end 
 			} // for2 end 
-			// 정상입력[통과가 true] 이면 배열에 저장하기 8
+			// 정상입력[통과가 true] 이면 배열에 저장하기 
 			if( 통과 == true) 사용자번호[i] = 번호;
 			
 		} // for end 
@@ -58,10 +58,63 @@ public class 로또뽑기 {// cs
 		}
 		
 		// 2. 컴퓨터가 난수생성 
+		for( int i =0 ; i<6 ; i++) {
+			Random random = new Random(); // 1. 난수객체
+			int 추첨 = random.nextInt(45)+1; // 2. 난수를 int형으로 1~45 사이 난수 가져오기
+			// 조건1 : 중복체크
+			boolean 통과 = true;
+			for( int temp : 추첨번호 ) {
+				if( 추첨 == temp ) {
+					i--; // 중복이 있을경우 현재 i 예외
+					통과 = false; // 저장 x 
+				}
+			}
+			if( 통과 ) 추첨번호[i] = 추첨;
+		}
+		System.out.print("\n \t추첨번호\t: ");
+		// * 배열내 모든 인덱스 출력 
+		for( int temp : 추첨번호 ) {
+			System.out.print( temp +"\t");
+		}
 		
 		// 3. 비교 = 두 배열간 동일한 수 찾기 
+			// 3 5 2
+			// 8 3 1
+		// 방법1
+		int 동일한수 = 0;
+//		for( int i=0;i<사용자번호.length;i++) {
+//			// i 비교 기준점
+//			for( int j = 0 ; j<추첨번호.length;j++) {
+//				//j 비교대상
+//				if(사용자번호[i] == 추첨번호[j]) {
+//					동일한수++;
+//				}
+//				
+//			}	
+//		}
+		// 방법2
+		for(int 비교기준: 사용자번호) {
+			//사용자번호 배열내 하나씩 비교기준 대입
+			for(int 비교대상: 추첨번호) {
+				//추첨번호 배열내 하나씩 비교대상 대입
+				if( 비교기준 == 비교대상) 동일한수++;
+			}
+		}
+		
+		System.out.println("\n"+"동일한 수: "+동일한수 );
 		
 				
 		
 	}// me
 } // c e 
+
+
+
+
+
+
+
+
+
+
+
