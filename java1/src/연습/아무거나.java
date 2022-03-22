ea04c664babd437527dd6d953629b992cb8230ab
0402park <redpyg39@gmail.com>
2022-03-22 09:32:32 +0900

    11

---
+++ java1/src/연습/아무거나.java
@@ -0,0 +1,13 @@
+package 연습;
+
+
+import java.util.Scanner;
+
+public class 아무거나 {
+    public static void main(String[] args) {
+    	int str = 0;
+        
+        System.out.println(str);
+    }
+
+}
